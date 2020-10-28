c0e7a70f50b6fbc77bc4a0fec10eb5f19e4417f3
Andy <alin21@stuy.edu>
2020-10-28 18:14:50 -0400

    added perimeter (i am using ttwo computers lol)

--- Triangles.java
+++ Triangles.java
@@ -10,4 +10,7 @@ public class Triangles{
     v2 = Point(x2,y2);
     v3 = Point(x3,y3);
   }
+  public double perimeter(Point a, Point b, Point c){
+    return(v1.distanceTo(v2)+v2.distanceTo(v3)+v3.distanceTo(v1));
+  }
 }
